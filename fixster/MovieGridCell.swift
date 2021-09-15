b02043b02dbd0e2acb86bbeb58c2b9d62d549382
Alfredo <icealfredo123@gmail.com>
2021-09-15 12:17:03 -0500

    commit

---
+++ fixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  fixster
+//
+//  Created by Alfredo Gutierrez on 9/14/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
